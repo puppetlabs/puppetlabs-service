2c19b5a0804a3d9bd397dec06af3bd68ca9657eb
tphoney <tp@puppet.com>
2017-09-14 11:53:09 +0100

    Remove bolt gem, test cleanup

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -1,48 +1,49 @@
 # run a test task
 require 'spec_helper_acceptance'
-
+# bolt regexes
+# expect_multiple_regexes(result: result, regexes: [%r{"status":"(stopped|in_sync)"}, %r{Ran on 1 node}])
+# expect_multiple_regexes(result: result, regexes: [%r{"status":"stopped"}, %r{"enabled":"false"}, %r{Ran on 1 node}])
 describe 'service task' do
   before(:all) do
     apply_manifest('package { "ntp": ensure => present, }')
-    sleep(6)
   end
   describe 'stop action' do
     it 'stop/status a service' do
-      result = run_puppet_task(task_name: 'service', params: { 'action' => 'stop', 'service' => 'ntpd' })
-      expect_multiple_regexes(result: result, regexes: [%r{status : in_sync}, %r{Job completed. 1/1 nodes succeeded}])
-      result = run_puppet_task(task_name: 'service', params: { 'action' => 'status', 'service' => 'ntpd' })
+      result = run_task(task_name: 'service', params: 'action=stop service=ntpd')
+      expect_multiple_regexes(result: result, regexes: [%r{status : (in_sync|stopped)}, %r{Job completed. 1/1 nodes succeeded}])
+      result = run_task(task_name: 'service', params: 'action=status service=ntpd')
       expect_multiple_regexes(result: result, regexes: [%r{status : stopped}, %r{enabled : false}, %r{Job completed. 1/1 nodes succeeded}])
     end
   end
   describe 'start action' do
     it 'start/status a service' do
-      result = run_puppet_task(task_name: 'service', params: { 'action' => 'start', 'service' => 'ntpd' })
+      result = run_task(task_name: 'service', params: 'action=start service=ntpd')
       expect_multiple_regexes(result: result, regexes: [%r{status : started}, %r{Job completed. 1/1 nodes succeeded}])
-      result = run_puppet_task(task_name: 'service', params: { 'action' => 'status', 'service' => 'ntpd' })
+      result = run_task(task_name: 'service', params: 'action=status service=ntpd')
       expect_multiple_regexes(result: result, regexes: [%r{status : running}, %r{enabled : false}, %r{Job completed. 1/1 nodes succeeded}])
     end
   end
   describe 'restart action' do
     it 'restart/status a service' do
-      result = run_puppet_task(task_name: 'service', params: { 'action' => 'restart', 'service' => 'ntpd' })
+      result = run_task(task_name: 'service', params: 'action=restart service=ntpd')
       expect_multiple_regexes(result: result, regexes: [%r{status : restarted}, %r{Job completed. 1/1 nodes succeeded}])
-      result = run_puppet_task(task_name: 'service', params: { 'action' => 'status', 'service' => 'ntpd' })
+      result = run_task(task_name: 'service', params: 'action=status service=ntpd')
       expect_multiple_regexes(result: result, regexes: [%r{status : running}, %r{enabled : false}, %r{Job completed. 1/1 nodes succeeded}])
     end
   end
   describe 'enable action' do
     it 'enable/status a service' do
-      result = run_puppet_task(task_name: 'service', params: { 'action' => 'enable', 'service' => 'ntpd' })
+      result = run_task(task_name: 'service', params: 'action=enable service=ntpd')
       expect_multiple_regexes(result: result, regexes: [%r{status : enabled}, %r{Job completed. 1/1 nodes succeeded}])
-      result = run_puppet_task(task_name: 'service', params: { 'action' => 'status', 'service' => 'ntpd' })
+      result = run_task(task_name: 'service', params: 'action=status service=ntpd')
       expect_multiple_regexes(result: result, regexes: [%r{status : running}, %r{enabled : true}, %r{Job completed. 1/1 nodes succeeded}])
     end
   end
   describe 'disable action' do
     it 'enable/status a service' do
-      result = run_puppet_task(task_name: 'service', params: { 'action' => 'disable', 'service' => 'ntpd' })
+      result = run_task(task_name: 'service', params: 'action=disable service=ntpd')
       expect_multiple_regexes(result: result, regexes: [%r{status : disabled}, %r{Job completed. 1/1 nodes succeeded}])
-      result = run_puppet_task(task_name: 'service', params: { 'action' => 'status', 'service' => 'ntpd' })
+      result = run_task(task_name: 'service', params: 'action=status service=ntpd')
       expect_multiple_regexes(result: result, regexes: [%r{status : running}, %r{enabled : false}, %r{Job completed. 1/1 nodes succeeded}])
     end
   end
