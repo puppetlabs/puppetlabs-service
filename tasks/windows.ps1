178bfc5f11b2b6c42803993b306154ce289cafe4
Iristyle <Iristyle@github>
2018-03-06 13:41:16 -0800

    (MODULES-6800) Add Windows service management
     - Basic ability to start / stop / restart and query status on
       a service given a name
    
     - Follows the same model as existing task where the state machine
       flows like:
    
       - status
         status: started
       - start
         status: in_sync
       - restart
         status: restarted
       - status
         status: started
       - stop
         status: stopped
    
       - status
         status: stopped
       - stop
         status: in_sync
       - restart
         status: restarted
    
       - status
         status: started
    
       NOTE: Windows `Running` status is converted to `started`
    
     - For states Puppet doesn't currently model, but that Windows will
       return, return those values:
    
       ContinuePending
       Paused
       PausePending
       StartPending
       StopPending
    
     - JSON payload is formatted with the following camelCase keys:
    
       name        : short service name
       action      : requested task action
       displayName : longer human friendly Windows service name
       status      : in_sync, started, stopped, restarted or other
                     afore-mentioned ServiceControllerStatus values
       startType   : a .NET ServiceStartMode value of Automatic, Boot,
                     Disabled, Manual or System

---
+++ tasks/windows.ps1
@@ -0,0 +1,60 @@
+[CmdletBinding()]
+param(
+  # NOTE: init.json cannot yet be shared, so must have windows.json / windows.ps1
+  [Parameter(Mandatory = $true)]
+  [String]
+  $Name,
+
+  [Parameter(Mandatory = $true)]
+  [ValidateSet('start', 'stop', 'restart', 'status')]
+  [String]
+  $Action
+)
+
+$ErrorActionPreference = 'Stop'
+
+$inSyncStatus = 'in_sync'
+$status = $null
+$service = Get-Service -Name $Name
+
+switch ($Action)
+{
+  'start'
+  {
+    if ($service.Status -eq 'Running') { $status = $InSyncStatus }
+    else { Start-Service $service }
+  }
+  'stop'
+  {
+    if ($service.Status -eq 'Stopped') { $status = $InSyncStatus }
+    else { Stop-Service $service }
+  }
+  'restart'
+  {
+    Restart-Service $service
+    $status = 'restarted'
+  }
+  # no-op since status always returned
+  'status' { }
+}
+
+# user action
+if ($status -eq $null)
+{
+  # https://msdn.microsoft.com/en-us/library/system.serviceprocess.servicecontrollerstatus(v=vs.110).aspx
+  # ContinuePending, Paused, PausePending, Running, StartPending, Stopped, StopPending
+  $status = $service.Status
+  if ($status -eq 'Running') { $status = 'started' }
+}
+
+# TODO: could use ConvertTo-Json, but that requires PS3
+# if embedding in literal, should make sure Name / Status doesn't need escaping
+Write-Host @"
+{
+  "name"        : "$($service.Name)",
+  "action"      : "$Action",
+  "displayName" : "$($service.DisplayName)",
+  "status"      : "$status",
+  "startType"   : "$($service.StartType)"
+}
+"@
