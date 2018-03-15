cfeb72025c3dabc1e9ab4c5fdb59ea527ba8efb7
tphoney <tp@puppet.com>
2018-03-15 11:44:38 +0000

    (enhancement) add restart to linux task

--- spec/acceptance/linux_spec.rb
+++ spec/acceptance/linux_spec.rb
@@ -25,4 +25,10 @@ describe 'linux service task', unless: fact_on(default, 'osfamily') == 'windows'
       expect_multiple_regexes(result: result, regexes: [%r{status.*(start)}, %r{#{task_summary_line}}])
     end
   end
+  describe 'restart action' do
+    it "restart #{package_to_use}" do
+      result = run_task(task_name: 'service::linux', params: "action=restart name=#{package_to_use}")
+      expect_multiple_regexes(result: result, regexes: [%r{status.*(restart)}, %r{#{task_summary_line}}])
+    end
+  end
 end
