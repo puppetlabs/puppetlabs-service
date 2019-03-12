2a41b9267bb6d28096781cb8364bc5a18405ffcd
lionce <loredana.ionce@puppet.com>
2019-03-12 11:35:22 +0200

    changed to call apply_manifest from beaker

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -12,10 +12,7 @@ describe 'service task' do
         task_run('service', 'action' => 'stop', 'name' => 'syslog')
       end
       package_to_use = 'rsyslog'
-      BoltSpec::Run.instance_methods(apply_manifest("package { \"#{package_to_use}\": ensure => present, }", 'default',
-                                                    execute: true,
-                                                    config: { 'modulepath' => RSpec.configuration.module_path },
-                                                    inventory: hosts_to_inventory.merge('features' => ['puppet-agent'])))
+      apply_manifest_on(default, "package { \"#{package_to_use}\": ensure => present, }")
     else
       package_to_use = 'W32Time'
       task_run('service', 'action' => 'start', 'name' => package_to_use)
--- spec/acceptance/linux_spec.rb
+++ spec/acceptance/linux_spec.rb
@@ -10,10 +10,7 @@ describe 'linux service task', unless: os[:family] == 'windows' do
     if os[:family] == 'redhat' && os[:release].to_i < 6
       task_run('service::linux', 'action' => 'stop', 'name' => 'syslog')
     end
-    BoltSpec::Run.instance_methods(apply_manifest("package { \"#{package_to_use}\": ensure => present, }", 'default',
-                                                  execute: true,
-                                                  config: { 'modulepath' => RSpec.configuration.module_path },
-                                                  inventory: hosts_to_inventory.merge('features' => ['puppet-agent'])))
+    apply_manifest_on(default, "package { \"#{package_to_use}\": ensure => present, }")
   end
 
   describe 'stop action' do
