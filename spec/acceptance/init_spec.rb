19e76e66efcb2d5d04f021a8f5c05015c5a873fa
lionce <42546087+lionce@users.noreply.github.com>
2019-03-12 12:40:07 +0200

    (FM-7798) Test audit making making testing more efficient  (#81)

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -5,45 +5,27 @@ describe 'service task' do
   include Beaker::TaskHelper::Inventory
   include BoltSpec::Run
 
-  def module_path
-    RSpec.configuration.module_path
-  end
-
-  def config
-    { 'modulepath' => module_path }
-  end
-
-  def inventory
-    hosts_to_inventory.merge('features' => ['puppet-agent'])
-  end
-
-  def run(params)
-    run_task('service', 'default', params, config: config, inventory: inventory)
-  end
-
-  osfamily_fact = fact('osfamily')
-
   package_to_use = ''
   before(:all) do
-    if osfamily_fact != 'windows'
-      if osfamily_fact == 'RedHat' && fact('operatingsystemrelease') < '6'
-        run('action' => 'stop', 'name' => 'syslog')
+    if os[:family] != 'windows'
+      if os[:family] == 'redhat' && os[:release].to_i < 6
+        task_run('service', 'action' => 'stop', 'name' => 'syslog')
       end
       package_to_use = 'rsyslog'
       apply_manifest_on(default, "package { \"#{package_to_use}\": ensure => present, }")
     else
       package_to_use = 'W32Time'
-      run('action' => 'start', 'name' => package_to_use)
+      task_run('service', 'action' => 'start', 'name' => package_to_use)
     end
   end
 
   describe 'enable action' do
     it 'enable/status a service' do
-      result = run('action' => 'enable', 'name' => package_to_use)
+      result = task_run('service', 'action' => 'enable', 'name' => package_to_use)
       expect(result[0]['status']).to eq('success')
       expect(result[0]['result']['status']).to match(%r{in_sync|enabled})
 
-      result = run('action' => 'status', 'name' => package_to_use)
+      result = task_run('service', 'action' => 'status', 'name' => package_to_use)
       expect(result[0]['status']).to eq('success')
       expect(result[0]['result']['enabled']).to eq('true')
     end
@@ -51,11 +33,11 @@ describe 'service task' do
 
   describe 'restart action' do
     it 'restart/status a service' do
-      result = run('action' => 'restart', 'name' => package_to_use)
+      result = task_run('service', 'action' => 'restart', 'name' => package_to_use)
       expect(result[0]['status']).to eq('success')
       expect(result[0]['result']['status']).to eq('restarted')
 
-      result = run('action' => 'status', 'name' => package_to_use)
+      result = task_run('service', 'action' => 'status', 'name' => package_to_use)
       expect(result[0]['status']).to eq('success')
       expect(result[0]['result']['status']).to eq('running')
       expect(result[0]['result']['enabled']).to eq('true')
@@ -64,13 +46,13 @@ describe 'service task' do
 
   describe 'stop action' do
     it 'stop/status a service' do
-      result = run('action' => 'stop', 'name' => package_to_use)
+      result = task_run('service', 'action' => 'stop', 'name' => package_to_use)
       expect(result[0]['status']).to eq('success')
       expect(result[0]['result']['status']).to match(%r{in_sync|stopped})
 
       # Debian can give incorrect status
-      if osfamily_fact != 'Debian'
-        result = run('action' => 'status', 'name' => package_to_use)
+      unless ['debian', 'ubuntu'].include?(os[:family])
+        result = task_run('service', 'action' => 'status', 'name' => package_to_use)
         expect(result[0]['status']).to eq('success')
         expect(result[0]['result']['status']).to eq('stopped')
         expect(result[0]['result']['enabled']).to eq('true')
@@ -80,13 +62,13 @@ describe 'service task' do
 
   describe 'start action' do
     it 'start/status a service' do
-      result = run('action' => 'start', 'name' => package_to_use)
+      result = task_run('service', 'action' => 'start', 'name' => package_to_use)
       expect(result[0]['status']).to eq('success')
       expect(result[0]['result']['status']).to match(%r{in_sync|started})
 
       # Debian can give incorrect status
-      if osfamily_fact != 'Debian'
-        result = run('action' => 'status', 'name' => package_to_use)
+      if os[:family] != 'debian'
+        result = task_run('service', 'action' => 'status', 'name' => package_to_use)
         expect(result[0]['status']).to eq('success')
         expect(result[0]['result']['status']).to eq('running')
         expect(result[0]['result']['enabled']).to eq('true')
@@ -96,11 +78,11 @@ describe 'service task' do
 
   describe 'disable action' do
     it 'disable/status a service' do
-      result = run('action' => 'disable', 'name' => package_to_use)
+      result = task_run('service', 'action' => 'disable', 'name' => package_to_use)
       expect(result[0]['status']).to eq('success')
       expect(result[0]['result']['status']).to eq('disabled')
 
-      result = run('action' => 'status', 'name' => package_to_use)
+      result = task_run('service', 'action' => 'status', 'name' => package_to_use)
       expect(result[0]['status']).to eq('success')
       expect(result[0]['result']['enabled']).to eq('false')
     end
--- spec/acceptance/linux_spec.rb
+++ spec/acceptance/linux_spec.rb
@@ -1,37 +1,21 @@
 # run a test task
 require 'spec_helper_acceptance'
 
-describe 'linux service task', unless: fact('osfamily') == 'windows' do
+describe 'linux service task', unless: os[:family] == 'windows' do
   include Beaker::TaskHelper::Inventory
   include BoltSpec::Run
 
-  def module_path
-    RSpec.configuration.module_path
-  end
-
-  def config
-    { 'modulepath' => module_path }
-  end
-
-  def inventory
-    hosts_to_inventory
-  end
-
-  def run(params)
-    run_task('service::linux', 'default', params, config: config, inventory: inventory)
-  end
-
   package_to_use = 'rsyslog'
   before(:all) do
-    if fact('osfamily') == 'RedHat' && fact('operatingsystemrelease') < '6'
-      run('action' => 'stop', 'name' => 'syslog')
+    if os[:family] == 'redhat' && os[:release].to_i < 6
+      task_run('service::linux', 'action' => 'stop', 'name' => 'syslog')
     end
     apply_manifest_on(default, "package { \"#{package_to_use}\": ensure => present, }")
   end
 
   describe 'stop action' do
     it "stop #{package_to_use}" do
-      result = run('action' => 'stop', 'name' => package_to_use)
+      result = task_run('service::linux', 'action' => 'stop', 'name' => package_to_use)
       expect(result[0]['status']).to eq('success')
       expect(result[0]['result']['status']).to match(%r{stop})
     end
@@ -39,7 +23,7 @@ describe 'linux service task', unless: fact('osfamily') == 'windows' do
 
   describe 'start action' do
     it "start #{package_to_use}" do
-      result = run('action' => 'start', 'name' => package_to_use)
+      result = task_run('service::linux', 'action' => 'start', 'name' => package_to_use)
       expect(result[0]['status']).to eq('success')
       expect(result[0]['result']['status']).to match(%r{start})
     end
@@ -47,7 +31,7 @@ describe 'linux service task', unless: fact('osfamily') == 'windows' do
 
   describe 'restart action' do
     it "restart #{package_to_use}" do
-      result = run('action' => 'restart', 'name' => package_to_use)
+      result = task_run('service::linux', 'action' => 'restart', 'name' => package_to_use)
       expect(result[0]['status']).to eq('success')
       expect(result[0]['result']['status']).to match(%r{restart})
     end
