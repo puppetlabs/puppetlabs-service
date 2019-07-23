25c035f621e46c0d65fcb0b48171c6ad7eb5a5f2
Michael T Lombardi <michael.t.lombardi@outlook.com>
2019-07-23 16:06:10 -0500

    (FM-8212) Convert to acceptance testing to litmus

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -2,98 +2,93 @@
 require 'spec_helper_acceptance'
 
 describe 'service task' do
-  include Beaker::TaskHelper::Inventory
-  include BoltSpec::Run
-
-  def bolt_config
-    { 'modulepath' => RSpec.configuration.module_path }
-  end
-
-  let(:bolt_inventory) { hosts_to_inventory.merge('features' => ['puppet-agent']) }
-
   package_to_use = ''
   before(:all) do
-    options = { inventory: hosts_to_inventory.merge('features' => ['puppet-agent']) }
+    unless ENV['TARGET_HOST'] == 'localhost'
+      inventory_hash = inventory_hash_from_inventory_file
+      add_feature_to_node(inventory_hash, 'puppet-agent', ENV['TARGET_HOST'])
+      write_to_inventory_file(inventory_hash, 'inventory.yaml')
+    end
     if os[:family] != 'windows'
       if os[:family] == 'redhat' && os[:release].to_i < 6
         params = { 'action' => 'stop', 'name' => 'syslog' }
-        run_task('service', 'default', params, options)
+        run_bolt_task('service', params)
       end
       package_to_use = 'rsyslog'
-      apply_manifest_on(default, "package { \"#{package_to_use}\": ensure => present, }")
+      apply_manifest("package { \"#{package_to_use}\": ensure => present, }")
     else
       package_to_use = 'W32Time'
       params = { 'action' => 'start', 'name' => package_to_use }
-      run_task('service', 'default', params, options)
+      run_bolt_task('service', params)
     end
   end
 
   describe 'enable action' do
     it 'enable/status a service' do
-      result = run_task('service', 'default', 'action' => 'enable', 'name' => package_to_use)
-      expect(result[0]).to include('status' => 'success')
-      expect(result[0]['result']).to include('status' => %r{in_sync|enabled})
+      result = run_bolt_task('service', 'action' => 'enable', 'name' => package_to_use)
+      expect(result.exit_code).to eq(0)
+      expect(result['result']).to include('status' => %r{in_sync|enabled})
 
-      result = run_task('service', 'default', 'action' => 'status', 'name' => package_to_use)
-      expect(result[0]).to include('status' => 'success')
-      expect(result[0]['result']).to include('enabled' => 'true')
+      result = run_bolt_task('service', 'action' => 'status', 'name' => package_to_use)
+      expect(result.exit_code).to eq(0)
+      expect(result['result']).to include('enabled' => 'true')
     end
   end
 
   describe 'restart action' do
     it 'restart/status a service' do
-      result = run_task('service', 'default', 'action' => 'restart', 'name' => package_to_use)
-      expect(result[0]).to include('status' => 'success')
-      expect(result[0]['result']).to include('status' => 'restarted')
+      result = run_bolt_task('service', 'action' => 'restart', 'name' => package_to_use)
+      expect(result.exit_code).to eq(0)
+      expect(result['result']).to include('status' => 'restarted')
 
-      result = run_task('service', 'default', 'action' => 'status', 'name' => package_to_use)
-      expect(result[0]).to include('status' => 'success')
-      expect(result[0]['result']).to include('status' => 'running')
-      expect(result[0]['result']).to include('enabled' => 'true')
+      result = run_bolt_task('service', 'action' => 'status', 'name' => package_to_use)
+      expect(result.exit_code).to eq(0)
+      expect(result['result']).to include('status' => 'running')
+      expect(result['result']).to include('enabled' => 'true')
     end
   end
 
   describe 'stop action' do
     it 'stop/status a service' do
-      result = run_task('service', 'default', 'action' => 'stop', 'name' => package_to_use)
-      expect(result[0]).to include('status' => 'success')
-      expect(result[0]['result']).to include('status' => %r{in_sync|stopped})
+      result = run_bolt_task('service', 'action' => 'stop', 'name' => package_to_use)
+      expect(result.exit_code).to eq(0)
+      expect(result['result']).to include('status' => %r{in_sync|stopped})
 
       # Debian can give incorrect status
       unless ['debian', 'ubuntu'].include?(os[:family])
-        result = run_task('service', 'default', 'action' => 'status', 'name' => package_to_use)
-        expect(result[0]).to include('status' => 'success')
-        expect(result[0]['result']).to include('status' => 'stopped')
-        expect(result[0]['result']).to include('enabled' => 'true')
+        result = run_bolt_task('service', 'action' => 'status', 'name' => package_to_use)
+        expect(result.exit_code).to eq(0)
+        expect(result['result']).to include('status' => 'stopped')
+        expect(result['result']).to include('enabled' => 'true')
       end
     end
   end
 
   describe 'start action' do
     it 'start/status a service' do
-      result = run_task('service', 'default', 'action' => 'start', 'name' => package_to_use)
-      expect(result[0]).to include('status' => 'success')
-      expect(result[0]['result']).to include('status' => %r{in_sync|started})
+      result = run_bolt_task('service', 'action' => 'start', 'name' => package_to_use)
+      expect(result.exit_code).to eq(0)
+      expect(result['result']).to include('status' => %r{in_sync|started})
 
       # Debian can give incorrect status
       if os[:family] != 'debian'
-        result = run_task('service', 'default', 'action' => 'status', 'name' => package_to_use)
-        expect(result[0]).to include('status' => 'success')
-        expect(result[0]['result']).to include('status' => 'running')
-        expect(result[0]['result']).to include('enabled' => 'true')
+        result = run_bolt_task('service', 'action' => 'status', 'name' => package_to_use)
+        expect(result.exit_code).to eq(0)
+        expect(result['result']).to include('status' => 'running')
+        expect(result['result']).to include('enabled' => 'true')
       end
     end
   end
 
   describe 'disable action' do
     it 'disable/status a service' do
-      result = run_task('service', 'default', 'action' => 'disable', 'name' => package_to_use)
-      expect(result[0]).to include('status' => 'success')
-      expect(result[0]['result']).to include('status' => 'disabled')
+      result = run_bolt_task('service', 'action' => 'disable', 'name' => package_to_use)
+      expect(result.exit_code).to eq(0)
+      expect(result['result']).to include('status' => 'disabled')
 
-      result = run_task('service', 'default', 'action' => 'status', 'name' => package_to_use)
-      expect(result[0]).to include('status' => 'success')
-      expect(result[0]['result']).to include('enabled' => 'false')
+      result = run_bolt_task('service', 'action' => 'status', 'name' => package_to_use)
+      expect(result.exit_code).to eq(0)
+      expect(result['result']).to include('enabled' => 'false')
     end
   end
 end
